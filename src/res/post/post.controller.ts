1712b369658d468f0b431873df5222c5a9f13c22
daindaind <cdin23@naver.com>
2024-08-04 18:20:30 +0900

    Feat: Post Controller 과제

--- src/res/post/post.controller.ts
+++ src/res/post/post.controller.ts
@@ -1,14 +1,52 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { PostService } from './post.service';
+
+type TPostList = {
+  nowPage: string;
+  pageCount: string;
+};
+
+export type TPost = {
+  id: number;
+  title: string;
+  content: string;
+};
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Post()
-  async postMainPage(): Promise<string> {
-    const res = await this.postService.postMainPage();
+  // @Post()
+  // async postMainPage(): Promise<string> {
+  //   const res = await this.postService.postMainPage();
 
-    return res;
+  //   return res;
+  // }
+
+  @Post()
+  async postContent(@Body() post: any): Promise<any> {
+    return {
+      title: post.title,
+      content: post.content,
+    };
+  }
+
+  @Get('/list')
+  async getPostList(
+    @Query('nowPage') nowPage: string,
+    @Query('pageCount') pageCount: string,
+  ): Promise<TPostList> {
+    return {
+      nowPage,
+      pageCount,
+    };
+  }
+
+  @Get('/:id')
+  async getPostById(@Param('id') id: string): Promise<TPost> {
+    const res = await this.postService.postData();
+
+    const post = res.filter((value) => value.id === parseInt(id));
+    return post[0];
   }
 }
--- src/res/post/post.service.ts
+++ src/res/post/post.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { TPost } from './post.controller';
 
 @Injectable()
 export class PostService {
   async postMainPage(): Promise<string> {
     return 'Post Main Page';
+  }
+
+  async postData(): Promise<TPost[]> {
+    return [
+      {
+        id: 1,
+        title: 'post 1',
+        content: 'post 1 content',
+      },
+      {
+        id: 2,
+        title: 'post 2',
+        content: 'post 2 content',
+      },
+      {
+        id: 3,
+        title: 'post 3',
+        content: 'post 3 content',
+      },
+    ];
   }
 }
